b9c87a855ceae7e1bb887fabea849283c695bd6f
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-08-31 20:07:33 +0700

    Add existing project files to Git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String args[]) {
-        System.out.println("Hellawww Everybodyyyy >_<");
+    public static void main(String[] args) {
+        int a=2;
+        String b="Test";
+        String c=a+b;
+        System.out.println(c); }
     }
-}
